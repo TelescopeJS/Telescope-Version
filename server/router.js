4dde5ac924ec24f9e217d51b71fd792e9fa715cb
Sacha Greif <sacha357@gmail.com>
2014-09-20 11:13:28 +0900

    storing more info; version bump

--- server/router.js
+++ server/router.js
@@ -4,21 +4,27 @@ Router.map(function() {
     path: '/',
     action: function(){
       var data = this.request.query;
-      // console.log(data)
+      console.log(data)
       this.response.setHeader('access-control-allow-origin', '*');
       this.response.setHeader('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept');
       this.response.write(latestVersion);
       this.response.end();
 
-      // log request
-      var upsert = Instances.upsert({siteUrl: data.siteUrl}, {
-        $set: {
-          siteUrl: data.siteUrl,
-          currentVersion: data.currentVersion,
-          siteTitle: data.siteTitle
-        }
-      });
-      // console.log(upsert)
+      // log request only if URL doesn't contain "localhost"
+      if(data.siteUrl.indexOf('localhost') == -1 && data.siteUrl.indexOf('0.0.0.0') == -1){
+        var upsert = Instances.upsert({siteUrl: data.siteUrl}, {
+          $set: {
+            siteUrl: data.siteUrl,
+            currentVersion: data.currentVersion,
+            siteTitle: data.siteTitle,
+            users: data.users,
+            posts: data.posts,
+            comments: data.comments,
+            lastSeenAt: new Date()
+          }
+        });
+        // console.log(upsert)
+      }
     }
   });
 });
