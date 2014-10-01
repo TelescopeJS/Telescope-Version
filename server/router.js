a5a66900ed73734e6ffc697a6af764cf4bca5b83
Sacha Greif <sacha357@gmail.com>
2014-10-01 11:36:32 +0900

    firstSeenAt

--- server/router.js
+++ server/router.js
@@ -12,18 +12,23 @@ Router.map(function() {
 
       // log request only if URL doesn't contain "localhost"
       if(data.siteUrl.indexOf('localhost') == -1 && data.siteUrl.indexOf('0.0.0.0') == -1){
-        var upsert = Instances.upsert({siteUrl: data.siteUrl}, {
-          $set: {
-            siteUrl: data.siteUrl,
-            currentVersion: data.currentVersion,
-            siteTitle: data.siteTitle,
-            users: data.users,
-            posts: data.posts,
-            comments: data.comments,
-            lastSeenAt: new Date()
-          }
-        });
-        // console.log(upsert)
+        var instance = Instances.findOne({siteUrl: data.siteUrl});
+        var properties = {
+          siteUrl: data.siteUrl,
+          currentVersion: data.currentVersion,
+          siteTitle: data.siteTitle,
+          users: data.users,
+          posts: data.posts,
+          comments: data.comments,
+          lastSeenAt: new Date()
+        }
+        if(!!instance){ // if this is not the first log, update
+          var result = Instances.update({_id: instance._id}, {$set: properties});
+        } else { // else, add firstSeenAt timestamp and insert
+          var properties.firstSeenAt = new Date();
+          var result = Instances.insert({$set: properties});
+        }
+        // console.log(result)
       }
     }
   });
